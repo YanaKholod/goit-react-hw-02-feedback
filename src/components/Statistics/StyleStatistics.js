819443fa343fae47589cc4acb8159f087f58a331
YanaKholod <106437450+YanaKholod@users.noreply.github.com>
2023-04-25 15:26:08 +0300

    hw

--- src/components/Statistics/StyleStatistics.js
+++ src/components/Statistics/StyleStatistics.js
@@ -1,15 +1,5 @@
 import styled from 'styled-components';
 
-const handleColorType = color => {
-  switch (color) {
-    case 'good':
-      return 'rgba(0, 128, 0, 25%)';
-    case 'neutral':
-      return 'rgba(255, 228, 196, 60%)';
-    case 'bad':
-      return 'rgba(255, 0, 0, 20%)';
-  }
-};
 export const Styled = {
   Numbers: styled.div`
     font-weight: 700;
